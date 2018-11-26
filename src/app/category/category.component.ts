1eefb45a42198d3104f7c8f59f4cc011150c1df2
Dhyan Raj <dhyan.raj@synechron.com>
2018-11-25 20:03:13 -0500

    Implemented Categories

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../data.service';
+
 
 @Component({
   selector: 'app-category',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  customers: Object;
+
+  platinum_arrow: boolean = true;
+  gold_arrow: boolean = false;
+  silver_arrow: boolean = false;
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getCutomers().subscribe(data => {
+      this.customers = data;
+      console.log(data);
+    });
+  }
+
+  platinumArrowClicked(){
+    this.platinum_arrow = !this.platinum_arrow;
+    this.gold_arrow = false;
+    this.silver_arrow = false;
+  }
+
+  goldArrowClicked(){
+    this.gold_arrow = !this.gold_arrow;
+    this.platinum_arrow = false;
+    this.silver_arrow = false;
+
+  }
+
+  silverArrowClicked(){
+    this.silver_arrow = !this.silver_arrow;
+    this.platinum_arrow = false;
+    this.gold_arrow = false;
   }
 
 }
